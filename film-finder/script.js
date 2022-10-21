6eac30058db3e15581c88bfa0be056d49761ceef
rockyrelay <81175151+rockyrelay@users.noreply.github.com>
2022-10-22 00:14:24 +0100

    holding bay for items ready for org repo

---
+++ film-finder/script.js
@@ -0,0 +1,6 @@
+const form = document.querySelector("form");
+const output = document.querySelector("output");
+const tmdbUrl = "https://api.themoviedb.org/3/search/movie?";
+const api = "api_key=4394d7ef7cb01b8b02c56df74ad20b06";
+
+
